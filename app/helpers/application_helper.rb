da11aab29158d3d991897a6ad8190ac925166f34
Orban Botond <orbanbotond@gmail.com>
2015-02-24 14:10:33 +0200

    Templates are nicer. Ability to add more accounts is possible.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def alert_class(k)
-    alerts = {:notice => 'alert-success', :alert => 'alert-warning', :error => 'alert-danger'}
+    alerts = {:success => 'alert-success', :notice => 'alert-success', :alert => 'alert-warning', :error => 'alert-danger'}
     alerts[k.to_sym]
   end
 
