78706f500fb6d80be7c90822f21af6e62c845133
Orban Botond <orbanbotond@gmail.com>
2015-03-05 15:14:13 +0200

    Chewy configuration is corrected.

--- config/initializers/chewy.rb
+++ config/initializers/chewy.rb
@@ -1,2 +1,2 @@
-Chewy.settings = {host: "#{ENV['SEARCHBOX_URL']}:9200"} if Rails.env.production?
+Chewy.configuration = {host: "#{ENV['SEARCHBOX_URL']}:9200"} if Rails.env.production?
 Chewy.urgent_update = true
