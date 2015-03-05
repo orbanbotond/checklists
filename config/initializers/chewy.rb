f9c630114a3b276dd5b7036ae1e806e13f2210fd
Orban Botond <orbanbotond@gmail.com>
2015-03-05 17:05:05 +0200

    Searchbox Url is corrected

--- config/initializers/chewy.rb
+++ config/initializers/chewy.rb
@@ -1,2 +1,2 @@
-Chewy.configuration = {host: "#{ENV['SEARCHBOX_URL']}:9200"} if Rails.env.production?
+Chewy.configuration = {host: "#{ENV['SEARCHBOX_URL']}"} if Rails.env.production?
 Chewy.urgent_update = true
