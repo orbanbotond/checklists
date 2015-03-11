97deb89355d14a654e3b717fa8fb65e5d87dfd46
Orban Botond <orbanbotond@gmail.com>
2015-03-11 10:59:40 +0200

    The smtp is set for the production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { :host => "#{ENV['DEPLOYED_DOMAIN']}.herokuapp.com" }
+  config.action_mailer.delivery_method = :smtp
+
 end
