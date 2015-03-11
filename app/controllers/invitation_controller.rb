8ba7a36f2b8f6342f637dc8ce7eeb89f0c1fa727
Orban Botond <orbanbotond@gmail.com>
2015-03-11 10:44:19 +0200

    Deprecated method is replaced. The sendgrid is user only in production mode

--- app/controllers/invitation_controller.rb
+++ app/controllers/invitation_controller.rb
@@ -8,7 +8,7 @@ class InvitationController < ApplicationController
       user = Subscribem::User.find_by_email( form.email) || Subscribem::User.invite!( email: form.email)
       current_account.users << user
       mail = UserMailer.added_to_account user, current_account
-      mail.deliver
+      mail.deliver_now
       redirect_to edit_subscribem_account_path(current_account), notice: 'The invitation is send and the user is added to the account'
     else
       redirect_to edit_subscribem_account_path(current_account), alert: 'The email must be a valid email'
